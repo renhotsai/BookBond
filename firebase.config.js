bcf47c5540af46e7cb53a1449ca0c3130f2fd587
Jeremy <renhotsai@gmail.com>
2024-05-17 19:02:56 -0400

    firebase setting

--- firebase.config.js
+++ firebase.config.js
@@ -19,3 +19,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, db, analytics };
