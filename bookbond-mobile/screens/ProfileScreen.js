eef8c508458f020165de52e121edbf66fa2919d3
Leo <leocesar_alcordo@hotmail.com>
2024-05-23 17:27:38 -0400

    added Leo test

--- bookbond-mobile/screens/ProfileScreen.js
+++ bookbond-mobile/screens/ProfileScreen.js
@@ -3,7 +3,7 @@ import { Text, View, TouchableOpacity } from "react-native";
 const ProfileScreen = (props) => {
   return (
     <View>
-      <Text>Sign Up Screen</Text>
+      <Text>Sign Up Screen LEO</Text>
       <TouchableOpacity onPress={props.logOut}>
         <Text style={{color: 'blue'}}>Sign Out</Text>
       </TouchableOpacity>
