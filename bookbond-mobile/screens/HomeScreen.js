4362184deffb3a5a6fd672f22665b20b1c01b604
Jeremy <renhotsai@gmail.com>
2024-05-23 16:27:31 -0400

    get data from api

--- bookbond-mobile/screens/HomeScreen.js
+++ bookbond-mobile/screens/HomeScreen.js
@@ -1,9 +1,53 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { FlatList, Text, View, Image, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
 
 const HomeScreen = () => {
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const [col, setCol] = useState([]);
+
+    const [booksFromAPI, setBooksFromAPI] = useState([]);
+    const fetchData = async () => {
+        const dataJson = await (await fetch(`https://www.googleapis.com/books/v1/volumes?q=""&startIndex=0&maxResults=40`)).json()
+        setBooksFromAPI(dataJson.items)
+    }
+
+    const onSearchPress = () => {
+        console.log(col);
+    }
+
+    const renderBookItem = (item) => {
+
+        console.log(item.volumeInfo.imageLinks.thumbnail);
+        return (
+            <View style={{ display: "flex", flexDirection: "row", padding: 5, gap: 10 }}>
+
+                <Image source={{ uri: `${item.volumeInfo.imageLinks.thumbnail}` }} style={{ width: 100, height: 100 }} />
+
+                <View style={{ justifyContent: "space-around" }}>
+                    <Text>{item.volumeInfo.title}</Text>
+                    <Text>{item.volumeInfo.authors}</Text>
+                </View>
+            </View>
+        )
+    }
+
     return (
         <View>
-            <Text>Home Screen</Text>
+            <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                <TextInput placeholder="search" />
+                <TouchableOpacity onPress={onSearchPress}>
+                    <AntDesign name="search1" size={24} color="black" />
+                </TouchableOpacity>
+            </View>
+            <FlatList
+                data={booksFromAPI}
+                renderItem={({ item }) => renderBookItem(item)}
+            />
         </View>
     );
 }
