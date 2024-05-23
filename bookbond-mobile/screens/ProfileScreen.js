9b4ede99194e8bfc72c39c24dd03c4bc32fed1f2
Jeremy <renhotsai@gmail.com>
2024-05-23 17:29:32 -0400

    jeremy test

--- bookbond-mobile/screens/ProfileScreen.js
+++ bookbond-mobile/screens/ProfileScreen.js
@@ -3,7 +3,7 @@ import { Text, View, TouchableOpacity } from "react-native";
 const ProfileScreen = (props) => {
   return (
     <View>
-      <Text>Sign Up Screen LEO</Text>
+      <Text>Sign Up Screen Jeremy</Text>
       <TouchableOpacity onPress={props.logOut}>
         <Text style={{color: 'blue'}}>Sign Out</Text>
       </TouchableOpacity>
