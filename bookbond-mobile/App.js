f2df89bf6382bcc87e6692456966b1ef8a445e54
Jeremy <renhotsai@gmail.com>
2024-05-24 00:23:36 -0400

    change screen flow

--- bookbond-mobile/App.js
+++ bookbond-mobile/App.js
@@ -1,24 +1,40 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import LoginScreen from "./screens/LoginScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
 import SignUpScreen from "./screens/SignUpScreen";
 import BookDetails from './screens/BookDetailScreen';
+import TabScreen from "./screens/TabScreen";
+import { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import StackScreen from "./screens/StackScreen";
 
 const App = () => {
   const Stack = createNativeStackNavigator();
+
+  const [currentScreen, setCurrentScreen] = useState("Login")
+
+  const changeScreen = (screen) => {
+    console.log(`changeScreen ${screen.screenName}`);
+    setCurrentScreen(screen.screenName);
+  }
+
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case 'Login':
+        return <LoginScreen screenChange={changeScreen} />;
+      case 'SignUp':
+        return <SignUpScreen screenChange={changeScreen} />;
+      case 'Main':
+        return <StackScreen screenChange={changeScreen} />;
+      default:
+        return <LoginScreen screenChange={changeScreen} />;
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name="SignUp" component={SignUpScreen} />
-          <Stack.Screen name="BookDetails" component={BookDetails} />
-        </Stack.Navigator>
+        {renderScreen()}
       </NavigationContainer>
     </SafeAreaView>
   );
---
+++ bookbond-mobile/screens/StackScreen.jsx
@@ -0,0 +1,30 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import TabScreen from './TabScreen'
+import BookDetailScreen from './BookDetailScreen'
+import { ScreenStackHeaderSearchBarView } from 'react-native-screens'
+import { Ionicons } from "@expo/vector-icons";
+import { auth } from '../firebaseConfig'
+
+const StackScreen = (props) => {
+    const Stack = createNativeStackNavigator()
+
+
+    const logout = () => {
+        auth.signOut();
+        props.screenChange({ screenName: "Login" })
+    }
+
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Main" options={{headerShown:false}}>
+                {()=><TabScreen logout={logout}/>}
+            </Stack.Screen>
+            <Stack.Screen name="BookDetails" component={BookDetailScreen} />
+        </Stack.Navigator>
+    )
+}
+
+export default StackScreen
