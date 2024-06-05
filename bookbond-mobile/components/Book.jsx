cb21306ecb3f915ae03fa4944fdf0d9bee8d01eb
Jeremy <renhotsai@gmail.com>
2024-06-04 23:11:09 -0400

    add collections in to user's collection

---
+++ bookbond-mobile/components/Book.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react'
+import { Text, View, Image, StyleSheet } from 'react-native';
+
+const Book = ({ item }) => (
+    <View style={styles.bookItem}>
+        {item.image && <Image source={{ uri: item.image }} style={styles.bookImage} />}
+        <View style={styles.bookInfo}>
+            <Text style={styles.bookTitle}>{item.title}</Text>
+            <Text style={styles.bookAuthors}>{item.authors}</Text>
+        </View>
+    </View>
+);
+
+export default Book
+
+
+
+const styles = StyleSheet.create({
+    bookItem: {
+        flex: 1,
+        padding: 20,
+        flexDirection: 'row',
+        marginBottom: 20,
+        backgroundColor: '#fff',
+        padding: 10,
+        borderRadius: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 2,
+        elevation: 2,
+    },
+    bookImage: {
+        width: 80,
+        height: 80,
+        marginRight: 10,
+    },
+    bookInfo: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    bookTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333',
+    },
+    bookAuthors: {
+        fontSize: 16,
+        color: '#666',
+    },
+});
--- bookbond-mobile/screens/CollectionScreen.jsx
+++ bookbond-mobile/screens/CollectionScreen.jsx
@@ -1,16 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { collection, doc, getDocs, onSnapshot, query } from 'firebase/firestore'
+import { auth, db } from '../firebaseConfig'
+import Book from '../components/Book'
+
+
+
 
 const CollectionScreen = () => {
+
+  useEffect(() => {
+    getBooksCollection()
+  }, [])
+
+  const [booksCollection, setBooksCollection] = useState({})
+
+  const getBooksCollection = async () => {
+    const user = auth.currentUser
+    if (user !== null) {
+      try {
+        const userDocRef = doc(db, 'UsersCollection', user.email);
+        const booksCollectionColRef = collection(userDocRef, 'BooksCollection')
+
+
+        const unsubscribe = onSnapshot(query(booksCollectionColRef), (querySnapshot) => {
+          const temp = []
+          querySnapshot.forEach((doc) => {
+            temp.push({
+              id: doc.id,
+              ...doc.data()
+            });
+          });
+
+          setBooksCollection(temp)
+
+          return () => unsubscribe();
+        });
+
+        // const books = await getDocs(booksCollectionColRef)
+        // if (books.size === 0) {
+        //   return
+        // }
+
+        // books.forEach((book) => {
+        //   // console.log(JSON.stringify(book.data()));
+        //   temp.push(book.data())
+        // }
+        // )
+      } catch (error) {
+        console.error("Error adding document: ", error);
+        Alert.alert("Error", "There was an error borrowing the book.");
+      }
+    } else {
+      Alert.alert("Not signed in", "You must be signed in to create a listing.");
+    }
+  }
+
+
+
   return (
-    <View>
-      <Text>CollectionScreen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={booksCollection}
+        renderItem={({ item }) => <Book item={item} />}
+        keyExtractor={(item, index) => index.toString()}
+        ListEmptyComponent={<Text>No collection books</Text>}
+      />
     </View>
   )
 }
 
-export default CollectionScreen
+
 
 const styles = StyleSheet.create({
-    
-})
+  container: {
+    backgroundColor: '#f8f8f8',
+    flex: 1,
+    padding: 20,
+  },
+});
+
+export default CollectionScreen
