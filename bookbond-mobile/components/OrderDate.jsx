dec4ba02e63444b70ee61c04e749179512bd4d61
Jeremy <renhotsai@gmail.com>
2024-07-18 17:42:19 -0400

    create order css

--- bookbond-mobile/components/OrderDate.jsx
+++ bookbond-mobile/components/OrderDate.jsx
@@ -1,12 +1,19 @@
+import { StyleSheet } from "react-native"
 import { Text, View } from "react-native"
 
 const OrderDate = ({ from, to }) => {
     return (
         <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-            <Text>{from}</Text>
-            <Text>-</Text>
-            <Text>{to}</Text>
+            <Text style={styles.dateTimeTxt}>{from}</Text>
+            <Text style={styles.dateTimeTxt}>-</Text>
+            <Text style={styles.dateTimeTxt}>{to}</Text>
         </View>
     )
 }
 export { OrderDate }
+
+const styles = StyleSheet.create({
+    dateTimeTxt: {
+        fontWeight: 'bold', fontSize: '20vm',
+    }
+})
